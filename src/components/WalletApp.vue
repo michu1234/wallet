7b8f52ec5ba04c049d7da191fef4572a543b9eae
unknown <dawiddeveloper@gmail.com>
2018-04-21 18:27:43 +0200

    scaffold

---
+++ src/components/WalletApp.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    {{msg}}
+    {{msg}}
+</div>
+</template>
+
+<script>
+  export default {
+      name: 'Wallet',
+      data() {
+          return {
+              msg: 'test msg'
+          }
+      },
+      medthods: {
+          
+      }
+  }
+
+</script>
+
+<style>
+
+
+</style>
+
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Wallet from '../components/WalletApp'
+
+Vue.use(Router)
+
+const routes = [
+  {
+    // path: '/sign-in',
+    path: '/',
+    name: 'SignIn',
+    component: () => import(/* webpackChunkName: "sign-in" */ 'src/views/SignIn'),
+  },
+  {
+    path: '/wallet',
+    name: 'Wallet',
+    component: Wallet
+  }
+]
+
+const router = new Router({
+  mode: 'history',
+  routes,
+})
+
+export default router
